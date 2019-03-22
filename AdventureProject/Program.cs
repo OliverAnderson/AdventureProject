a543d3ba25446a9e74b50a227780828ef6b0f539
oanderson <oanderson@umassd.edu>
2019-03-22 12:05:08 -0400

    Add project files.

---
+++ AdventureProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventureProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
